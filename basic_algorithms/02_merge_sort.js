0547c01a4d86134efd4742fe3815006ffac86509
Agility6 <agility6@163.com>
2022-12-15 01:55:50 +0800

    perf use splice

--- basic_algorithms/02_merge_sort.js
+++ basic_algorithms/02_merge_sort.js
@@ -2,7 +2,7 @@
  * merge_sort_template
  */
 function merge_sort(array, left, right) {
-
+  
   if(left >= right) return
 
   const mid = left + right >> 1
@@ -17,12 +17,9 @@ function merge_sort(array, left, right) {
     if(array[i] <= array[j]) tem.push(array[i++])
     else tem.push(array[j++])
   }
-  
+
   while(i <= mid) tem.push(array[i++])
   while(j <= right) tem.push(array[j++])
 
-  for(i = left, j = 0; j < tem.length; i++, j++) {
-    array[i] = tem[j]
-  }
-
+  array.splice(left, tem.length, ...tem)
 }
